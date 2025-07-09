545c66e98f0bebbdb9bed9075a2e37157e3377a6
Mikael Andreasyan <mikaelandreasyan@gmail.com>
2025-07-09 13:46:38 -0400

    Basic Controls
    -Clicking and dragging the mouse will cause player to launch opposite direction on release

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,20 +2,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] Rigidbody2D playerRb;
+    Vector3 originalPos;
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            originalPos = Input.mousePosition;
+            //store initial mouse location
+        }
+        if (Input.GetMouseButton(0))
+        {
+            
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            float xChange = -(Input.mousePosition.x - originalPos.x) / 10;
+            float yChange = -(Input.mousePosition.y - originalPos.y) / 10;
+            playerRb.linearVelocity = new Vector2(xChange, yChange);
+        }
         //get if the mouse was clicked down
         //update the force based on location of mouse in comparison with original location
-        //Input.mousePosition
         //Camera.main.ScreenToWorldPoint()
-
         //when let go, do a calculation and apply the force
     }
 }
