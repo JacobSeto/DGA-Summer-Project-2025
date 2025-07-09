c053b71f5f0c4d91233eb9adcd3431739f577401
MarieMoments335 <marieroberts0705@gmail.com>
2025-07-09 14:28:27 -0400

    Update GameManagerScript.cs

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -2,6 +2,12 @@ using System;
 using UnityEngine;
 using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
+/*
+ * The Game Manager handles win and loss conditions alongside tracking the time elapsed in each level.
+ * Variables:
+ * Loss: boolean that tracks if the player has lost the game by losing momentum
+ * Win: boolean that tracks if the player has beaten the level.
+ */
 public class GameManagerScript
 {
     private bool loss = false;
@@ -21,6 +27,7 @@ public class GameManagerScript
             Console.WriteLine(win);
         }
     }
+    //Tracks if the player has lost momentum as of now
     private bool isFrozen()
     {
         if (Velocity == 0 & isLaunched == true)
@@ -33,6 +40,7 @@ public class GameManagerScript
         Console.WriteLine("not loss");
         return false;
     }
+    //Returns if the player has won.
     public bool isWin()
     {
         if (win == true)
@@ -41,6 +49,7 @@ public class GameManagerScript
         }
         return false;
     }
+    //Returns if the player has lost.
     public bool isLose()
     {
         if (loss == true)
@@ -48,6 +57,12 @@ public class GameManagerScript
             return true;
         }
         return false;
+    }
+    //Resets key variables during level reloads or changes.
+    public void managerReset()
+    {
+        loss = false;
+        win = false;
     }
 }
 
