decb4300283b7c53b70365436643ad2ab49323ad
Angel <thesparkingofhope@gmail.com>
2025-07-09 14:29:59 -0400

    Add Documentation
    - Adds Documentation

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -49,18 +49,18 @@ public class CameraController : MonoBehaviour
             zoomed += 1;
             if (zoomed == 4) { zoomed = 1; }
         }
-        if (zoomed==1)
+        if (zoomed==1) // Following Player
         {
             currentSize -= (levelSize - playerSize) / 10;
             currentPosition = new Vector3(player.position.x, player.position.y, -10f);
         }
         
-        if (zoomed == 2)
+        if (zoomed == 2) // Whole Level overview
         {
             currentSize += (levelSize - playerSize) / 10;
             currentPosition = new Vector3(levelView.x, levelView.y, -10f);
         }
-        if (zoomed == 3)
+        if (zoomed == 3) // Inspecting Level
         {
             currentSize -= (levelSize - zoomScrollSize) / 10;
             
@@ -69,12 +69,14 @@ public class CameraController : MonoBehaviour
 
         currentSize = Mathf.Clamp(currentSize, playerSize, levelSize);
         _camera.orthographicSize = Mathf.SmoothDamp(_camera.orthographicSize, currentSize, ref velocity, smoothing);
+        //Moving Camera to and from player (This needs to be changed to incorperate player Speed aka Zoomed=1 )
         if (zoomed != 3)
         {
             transform.position = Vector3.Slerp(transform.position, currentPosition, movementSpeed);
         }
         else 
         {
+            //Camera Scrolling
             if (Input.GetButton("Fire1"))
             {
                 Vector3 camPos = transform.position;
