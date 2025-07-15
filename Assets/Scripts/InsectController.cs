b291396e9acc20eb17d4a9c4fab6f3144c037c82
sharonxz <ssharonzhengg@gmail.com>
2025-07-15 14:21:06 -0400

    added new insect sprite with animation

--- Assets/Scripts/InsectController.cs
+++ Assets/Scripts/InsectController.cs
@@ -11,7 +11,7 @@ public class InsectController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            // Change the player's stamina
+          // add stamina player code
         }
 
     }
