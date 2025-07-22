99d987cd87615d77cb51b02dd2fc1192a09d532f
af1799 <annabelle.ford.314@gmail.com>
2025-07-23 03:01:50 +0800

    created basic tutorial

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,33 @@
+using TMPro;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    private TMP_Text text;
+    private int clicks = 0;
+    private PlayerController player;
+    void Start()
+    {
+        text = gameObject.transform.GetChild(0).GetComponent<TMP_Text>();
+        player = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    void Update()
+    {
+        if (clicks == 0)
+        {
+            player.Freeze();
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            clicks = clicks + 1;
+        }
+        if (clicks == 1)
+        {
+            text.SetText("Launch Josephine towards the zookeeper by clicking " +
+                "and dragging your cursor until the arrow is pointing the " +
+                "direction you want to go. Then, release! Try it out now!");
+            player.Unfreeze();
+        }
+    }
+}
