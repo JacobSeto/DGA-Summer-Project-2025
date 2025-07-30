5ced0b2a6be4a73fc737a5b8e137b94fe3b199c9
chvelez19 <chvelez19@gmail.com>
2025-07-30 14:46:50 -0400

    Zookeeper Directional Indicator Changing Size

--- Assets/Scripts/ZookeeperDirection.cs
+++ Assets/Scripts/ZookeeperDirection.cs
@@ -4,6 +4,10 @@ public class ZookeeperDirection : MonoBehaviour
 {
     Transform nearestZookeeper;
     PlayerController player;
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] float minSize = 0;
+    [SerializeField] float maxXSize = 2;
+    [SerializeField] float maxYSize = 1.75f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,9 +20,34 @@ public class ZookeeperDirection : MonoBehaviour
     {
         nearestZookeeper = player.GetClosestZookeeper(GameManagerScript.Instance.GetZookeeperList());
 
-        var dir = nearestZookeeper.position - transform.position;
+        if (nearestZookeeper != null){
+            var dir = nearestZookeeper.position - transform.position;
 
-        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            float newX = Mathf.Abs(dir.magnitude) / 26.25f;
+            float newY = Mathf.Abs(dir.magnitude) / 30;
+
+            if (newX < minSize)
+            {
+                newX = minSize;
+            }
+            else if (newX > maxXSize)
+            {
+                newX = maxXSize;
+            }
+
+            if (newY < minSize)
+            {
+                newY = minSize;
+            }
+            else if (newY > maxYSize)
+            {
+                newY = maxYSize;
+            }
+
+            spriteRenderer.transform.localScale = new Vector3 (newX, newY, 1);
+
+            var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
     }
 }
