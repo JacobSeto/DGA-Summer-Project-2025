6f369943497e2efbaf3ce8fa2316aa0a1a9e62f8
af1799 <annabelle.ford.314@gmail.com>
2025-07-30 22:43:18 +0800

    timer logic added

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,6 +1,44 @@
+using System;
+using TMPro;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-    
+    private float timer;
+    private float timerGap;
+    private float minutes;
+    private float seconds;
+    private String secString;
+    private TMP_Text text;
+    private PlayerController player;
+    private bool firstLaunch = true;
+    void Start()
+    {
+        text = gameObject.transform.GetChild(0).GetComponent<TMP_Text>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    void Update()
+    {
+        if (player.launched && firstLaunch)
+        {
+            timerGap = (float)Math.Round(GameManagerScript.Instance.timer, 0);
+            firstLaunch = false;
+        }
+        else if (player.launched && !firstLaunch)
+        {
+            timer = (float)Math.Round(GameManagerScript.Instance.timer, 0) - timerGap;
+            if (timer / 60 >= 1)
+            {
+                minutes = timer / 60;
+            }
+            seconds = timer % 60;
+            secString = seconds.ToString();
+            if (secString.Length != 2)
+            {
+                secString = "0" + secString;
+            }
+            text.SetText(minutes.ToString() + ":" + secString);
+        }
+    }
 }
