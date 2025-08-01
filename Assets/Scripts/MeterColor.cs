50cd07bc5f16edcc8051b7150f6b91359b9d731f
af1799 <annabelle.ford.314@gmail.com>
2025-08-01 09:15:04 -0500

    speedometer

--- Assets/Scripts/MeterColor.cs
+++ Assets/Scripts/MeterColor.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,8 +9,11 @@ public class MeterColor : MonoBehaviour
     GameObject red;
     GameObject yellow;
     GameObject green;
+    GameObject blue;
     PlayerController player;
     [SerializeField] Pivot pivot;
+    private int redColor;
+    private Animator animator;
     void Start()
     {
         normal = gameObject.transform.GetChild(0).gameObject;
@@ -19,8 +23,11 @@ public class MeterColor : MonoBehaviour
         yellow.SetActive(false);
         green = gameObject.transform.GetChild(3).gameObject;
         green.SetActive(false);
+        blue = gameObject.transform.GetChild(4).gameObject;
+        blue.SetActive(false);
         current = normal;
         player = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+        animator = gameObject.GetComponent<Animator>();
     }
 
     void Update()
@@ -29,19 +36,16 @@ public class MeterColor : MonoBehaviour
         {
             if (pivot.angle >= 35)
             {
-                //red highlight
-                current.SetActive(false);
-                red.SetActive(true);
-                current = red;
+                animator.SetBool("Flashing", true);
             }
-        else if (pivot.angle >= -35)
+            else if (pivot.angle >= -35)
             {
                 //yellow
                 current.SetActive(false);
                 yellow.SetActive(true);
                 current = yellow;
             }
-        else
+            else
             {
                 //green
                 current.SetActive(false);
